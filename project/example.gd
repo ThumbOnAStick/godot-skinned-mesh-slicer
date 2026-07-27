148c30e7ebee1ed8f9253425b9ec180604d532a0
ThumbStick <120911598+ThumbOnAStick@users.noreply.github.com>
2026-07-27 17:24:04 +0800

    impl(thumb): Completed initial setup

--- project/example.gd
+++ project/example.gd
@@ -2,5 +2,8 @@ extends Node
 
 
 func _ready() -> void:
-	var example := ExampleClass.new()
-	example.print_type(example)
+	var summator = Summator.new()
+	summator.add(3)
+	summator.add(4)
+	print(summator.get_total())
+	
